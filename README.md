df142267ecb6b0e570a95d7f8854cdf29990d2dc
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2023-12-13 14:42:37 +0000

    Updated example env

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ bun run dev
 ```
 open http://localhost:3000
 ```
+
+I was gonna use this for the backend of a school project but I ended up changing my project idea, hence, never completed this.
--- src/ui/components/layout.tsx
+++ src/ui/components/layout.tsx
@@ -7,7 +7,6 @@ type Props = {
 
 export default async function Layout({ children, title }: Props) {
   const { isLoggedIn, user } = await useAuth();
-  console.log(user);
 
   return (
     <html lang="en">
@@ -15,10 +14,23 @@ export default async function Layout({ children, title }: Props) {
         <meta charset="utf-8" />
         <title>{title}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <script src="//unpkg.com/alpinejs" defer></script>
         <link rel="stylesheet" href="css/style.css" />
       </head>
       <body>
-        {isLoggedIn ? <nav></nav> : null}
+        {isLoggedIn ? (
+          <nav class="flex justify-end py-3.5 px-6 w-screen border-b border-b-zinc-200">
+            <div class="flex gap-3 items-center">
+              <div class="overflow-hidden w-8 rounded-full aspect-square">
+                {user?.avatarUrl ? <img src={user?.avatarUrl} alt="avatar" /> : <div class="flex w-full h-full border bg-zinc-200" />}
+              </div>
+
+              <a href="/api/auth/sign-out" class="text-xs font-medium text-zinc-500 hover:text-zinc-600">
+                Sign out
+              </a>
+            </div>
+          </nav>
+        ) : null}
         <>{children}</>
       </body>
     </html>
